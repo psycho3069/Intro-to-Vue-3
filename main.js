84c45596024c7af1637de99749fa3c0aefe8c32b
psycho3069 <farha100669@gmail.com>
2021-02-09 08:15:18 +0600

    product details

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data(){
         return {
-            Product: 'Socks'
+            Product: 'Socks',
+            ProductDetails: 'I hate socks 😒😒'
         }
     }
 })
